2d2b206c129a4013e2222f925f8e74e5f2a43e86
tjpel <tjp00004@gmail.com>
2023-08-24 12:03:27 -0500

    Post-Lunch Update

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import plotly.express as px
 warnings.filterwarnings("ignore")
 
 st.title("Netflix Analysis")
-st.write("Hello evryone this is are site called the fuzzy fries and the piont of this site is to edjucate are selves and others on web creation  ")
+st.write("") #put bios here
+st.write("Hello everyone! This is our site called the Fuzzy Fries and the point of this site is to educate ourselves and others on data analysis. The reason why we chose a data set on Netflix is because it is what interested us the most. We want to know if our hypothesis on whether or not people's age affects what kind of subscription they buy is correct.")
 netflix = pd.read_csv('https://raw.githubusercontent.com/Kilroy11511/FuzzyFries/main/Netflix%20Userbase.csv')
 
 netflix.drop(["Plan Duration"], axis=1, inplace=True) #only one value in column, irrelevant
@@ -29,30 +30,30 @@ netflix['Binned Ages'] = pd.cut(netflix['Age'], bins=ageBins, right=True, labels
 
 basicDF = netflix[netflix['Subscription Type'] == "Basic"]
 basicDFVC = basicDF['Binned Ages'].value_counts()
-fig = px.pie(basicDFVC, values=basicDFVC.values, names=basicDFVC.index, title='Ages')
+fig = px.pie(basicDFVC, values=basicDFVC.values, names=basicDFVC.index, title='Basic Plan')
 st.plotly_chart(fig)
 
 standardDF = netflix[netflix['Subscription Type'] == "Standard"]
 standardDFVC = standardDF['Binned Ages'].value_counts()
-fig = px.pie(standardDF, values=standardDFVC.values, names=standardDFVC.index)
+fig = px.pie(standardDF, values=standardDFVC.values, names=standardDFVC.index, title='Standard Plan')
 st.plotly_chart(fig)
 
 premiumDF = netflix[netflix['Subscription Type'] == "Premium"]
 premiumDFVC = premiumDF['Binned Ages'].value_counts()
-fig = px.pie(premiumDF, values=premiumDFVC.values, names=premiumDFVC.index)
+fig = px.pie(premiumDF, values=premiumDFVC.values, names=premiumDFVC.index,'Premium Plan')
 st.plotly_chart(fig)
 
-st.write("The pie charts show that as age increases show does the likelihood of them subscribing to a plan however after the age of 50 this chance and likelihood drops sharply for a yet unknown reason.")
+st.write("The pie charts show that as age increases, the likelihood of them subscribing to a plan however after the age of 50 this chance and likelihood drops sharply for a yet unknown reason.")
 
 #for bar chart
 netflixGrouped = netflix.groupby(['Subscription Type']).mean(numeric_only=True)['Age']
-fig = px.bar(netflix, x=netflixGrouped.index, y =netflixGrouped.values)
+fig = px.bar(netflix, x=netflixGrouped.index, y =netflixGrouped.values, labels={'x':'Subscription Type', 'y': 'Age'}, title='Mean Age By Subscription Type')
 st.plotly_chart(fig)
 
 st.write('This graph represents the subscription types of Netflix users based on their ages. For example, the "Basic" subscription age average is 38.82883, the "Premium" subscription is 38.51296, "Standard" subscription is 39.02214.')
 
 #for violin plot
-fig = px.violin(netflix, y='Age', x='Subscription Type')
+fig = px.violin(netflix, y='Age', x='Subscription Type', title='Distribution of Concentration of Age with Subscription Type')
 st.plotly_chart(fig)
 
 st.write('This violin plot shows that the "Basic" and "Premium" subscription users are mostly 30 years old. For the "Standard" subscription users, they are in the 40-45 year old range.')
@@ -62,13 +63,13 @@ st.write('This violin plot shows that the "Basic" and "Premium" subscription use
 subTypeandAgeVC = netflix.value_counts(['Subscription Type', 'Binned Ages'])
 subTypeandAgeDF=subTypeandAgeVC.rename_axis(['Subscription Type', 'Binned Ages']).reset_index(name="Counts")
 
-fig = px.bar(subTypeandAgeDF, x='Binned Ages', y='Counts', color='Subscription Type')
+fig = px.bar(subTypeandAgeDF, x='Binned Ages', y='Counts', color='Subscription Type', title="Proportions of Subscription Types to Ages")
 st.plotly_chart(fig)
 
-st.write("This is a long format data box chart that shows the subscription type amount for each age group. For ages 35- 40 has the most people using the basic plan and 30 - 35 has the most people using the standerd and premium subcription plan.")
+st.write("This is a long format data box chart that shows the subscription type amount for each age group. For ages 35-40, most people using the basic plan and 30 - 35 has the most people using the standard and premium subscription plan.")
 
 #for box plot
-fig = px.box(netflix, y="Age", x='Subscription Type')
+fig = px.box(netflix, y="Age", x='Subscription Type', title='Age and Subscription')
 st.plotly_chart(fig)
 
-st.write("This box plot shows the age to subscription type  values. For example the youngest person to use the basic subscription type was 27 years old and the oldest was 45 and the one in the middle was 39.")
+st.write("This box plot shows the age to subscription type values. For example, the youngest person to use the basic subscription type was 27 years old and the oldest was 45 and the one in the middle was 39.")
